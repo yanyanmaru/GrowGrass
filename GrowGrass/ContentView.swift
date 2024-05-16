718b923f0ed080f27f3918886878871efa96ef92
yanyanmaru <manmarutech@gmail.com>
2024-05-16 10:42:15 +0900

    Initial Commit

---
+++ GrowGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrowGrass
+//
+//  Created by 平山奈々海 on 2024/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrowGrass/GrowGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowGrassApp.swift
+//  GrowGrass
+//
+//  Created by 平山奈々海 on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct GrowGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
